d2f4b951af4e98fa79bf112f5f85e5987a8b6a76
Mihai Listov <mihai.listov@hootsuite.com>
2017-08-21 09:54:32 -0700

    created post class

--- drone.py
+++ drone.py
@@ -16,20 +16,8 @@ import logging
 import os
 import sys
 
-#python3
-# from importlib import reload
-
 # defines whether to use test or actual login
 TEST_MODE = False
-
-# class Post:
-# 	def __init__(self, title):
-#         self.id = id(self)
-#         self.title = title
-#         self.fileName = fileName
-#         self.caption = caption
-#         self.comment = comment
-#         self.scheduledTime = scheduledTime
 
 class InstaDrone:
 
@@ -77,7 +65,7 @@ class InstaDrone:
 		loginButton = self.driver.find_element_by_xpath("//button[text()='Log in']")
 		loginButton.click()
 
-	def post(self, fileName, caption):
+	def post(self, post):
 		# click camera btn
 		cameraXPath = "//div[contains(@class, 'Camera')]"
 		self.wait.until(lambda driver: self.driver.find_element_by_xpath(cameraXPath))
@@ -88,7 +76,7 @@ class InstaDrone:
 
 		# select file in browser
 		sleep(0.5)
-		pyautogui.typewrite(fileName)
+		pyautogui.typewrite(post.fileName)
 		sleep(0.5)
 		pyautogui.keyDown('enter')
 		pyautogui.keyUp('enter')
@@ -118,7 +106,7 @@ class InstaDrone:
 	
 		# convert to unicode
 		# text = text.replace("'", "\\'")  # escape single quotes
-		caption = caption.encode('utf-8')  # needed to make format function work
+		caption = post.caption.encode('utf-8')  # needed to make format function work
 		captionArea.click()
 		sleep(1)
 		self.driver.execute_script("arguments[0].value = arguments[1]", captionArea, caption)
@@ -133,7 +121,11 @@ class InstaDrone:
 		shareButton = self.driver.find_element_by_xpath(shareXPath)
 		shareButton.click()
 
-	def commentPost(self, comment):
+		self.commentPost(post)
+
+		print('Successfully posted: ' + post.title + ' at time: ' + post.scheduledTime)
+
+	def commentPost(self, post):
 		# go to Profile
 		profileXPath = "//div[contains(@class, 'Profile')]"
 		self.wait.until(lambda driver: self.driver.find_element_by_xpath(profileXPath))
@@ -163,7 +155,7 @@ class InstaDrone:
 		self.driver.execute_script("arguments[0].scrollIntoView()", commentArea)
 		# commentArea.send_keys(comment)
 
-		comment = comment.encode('utf-8')  # needed to make format function work
+		comment = post.comment.encode('utf-8')  # needed to make format function work
 		sleep(1)
 		self.driver.execute_script("arguments[0].value = arguments[1]", commentArea, comment)
 		pyautogui.typewrite(' ')
@@ -172,60 +164,79 @@ class InstaDrone:
 		pyautogui.keyDown('enter')
 		pyautogui.keyUp('enter')
 
+		print('Successfully commented on post: ' + post.title + ' at time: ' + post.scheduledTime)
+
 	def halt(self):
-		print('shutting down...')
+		print('Autopost job complete')
 		sleep(3)
 		self.mobileBrowser.tearDown()
 
-def my_job(fileName=None, caption=None, comment=None):
+def autopost(post=None):
     drone = InstaDrone()
     drone.login()
-    drone.post(fileName, caption)
-    drone.commentPost(comment)
+    # drone.post(post)
+    drone.commentPost(post)
     drone.halt()
+
+posts = []
+
+class Post:
+	def __init__(self, title):
+		self.id = id(self)
+		self.title = title
+		self.fileName = None
+		self.caption = None
+		self.comment = None
+		self.scheduledTime = None
+		posts.append(self)
 
 if __name__ == "__main__":
 	sched = BlockingScheduler()
-
-	fileName = ['1.jpg', '2.jpeg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.JPG', '8.jpg', '9.jpg', '10.JPG']
-	# text = "28 GRAM GIVEAWAY 🍯  💎  ❤️\nCOMING UP ON AUGUST 28TH #28gOnThe28th\nThis is the 3RD post! 3RD chance to enter!\n14g Shatter 🍯  and 14g CBD Crystalline 💎 \n👇  CONTEST RULES (MUST fullfill 1️⃣  - 3️⃣ )👇\n1️⃣. FOLLOW @boutiquecannabiscanada 👀 \n2️⃣. REPOST this picture, make sure to tag us \n3️⃣. LIKE & COMMENT below, tag friends you'd smoke with 💨\nThe more friends you tag, the better your chances of winning 😀\nDM us anything you'd like to repost, we love original content 👌\nWith ❤️  from @boutiquecannabiscanada 👀 "
 	caption = [
-			"28 GRAM GIVEAWAY 🍯 💎 ❤️\nEVERY POST IS A NEW CHANCE TO WIN!\nCOMING UP ON AUGUST 28TH #28gOnThe28th\nPRIZE 👉 14g Shatter 🍯🐝 and 14g CBD Crystalline 💎💎 👇  CONTEST RULES (MUST complete all three )👇\n1️⃣. FOLLOW @boutiquecannabiscanada 👀 \n2️⃣. REPOST this picture, make sure to tag us \n3️⃣. LIKE & COMMENT below, tag friends you'd smoke with 💨\nMore friends you tag, the better your chances of winning 😀\nDM us anything to repost, we love original content 👌\nWith ❤️ from @boutiquecannabiscanada 👀",
-			"MADE IT TO 1K FOLLOWERS! ❤️  \nAs a thank you to all our followers we're running a sale on our website right now, use code: FUNAUG  ❤️  \nPeep @boutiquecannabiscanada 👀",
-			"Some OC from @triscuit.farms 😍 \nGiveaway going on right now 👉 14g Shatter 🍯🐝 and 14g CBD Crystalline 💎💎\n Check out our bio @boutiquecannabiscanada 👀  \nand all girls page @boutiquecannabisofficial 👀",
-			"Shout out to @cannaprincess1990 😍 \nGiveaway going on right now 👉 14g Shatter 🍯🐝 and 14g CBD Crystalline 💎💎\n Check out our bio @boutiquecannabiscanada 👀  \nand all girls page @boutiquecannabisofficial 👀",
-			"🤔🤔🤔 GUESS the STRAIN & TAG A FRIEND to WIN $100 store credit 🎁🎁🎁 drop by every week for our weekly #guesswhatwednesdays giveaway!! Shout out to @triscuit.farms Peep us @boutiquecannabiscanada 👀",
-			"#GUESSWHATWEDNESDAY WINNER @dylanthiessen52 🎁 🎁  There were multiple correct guesses, so we picked the first ! Come back next week, they'll be more prizes and more winners 👈 💯 \n🎁 🎁  Giveaway going on right now 👉 14g Shatter 🍯🐝 and 14g CBD Crystalline 💎💎  Check out @boutiquecannabiscanada 👀",
-			"Check out our delicious edibles 👅 😛 😋 💦  Peep our bio @boutiquecannabiscanada",
-			"September 2nd at @theplanetparadise is going to be an amazing evening 🎀 thank you to everyone who's contributing ❤️ @boutiquecannabiscanada 👀",
-			"Canada's most alluring women, Boutique Cannabis Girls. \nCheck out: @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada \nBoutique Cannabis girl: @kwbabyy @mzkaylzz @allthingsamandaa @j.desireexo @apriliciouss @marishika_ \nShooter: @jasegraphics\nMUA: @swankmakeup",
-			"Wise words from Carl Sagan 😂 😂 What's everyone smoking on today?"
+			"Canada's most alluring women, Boutique Cannabis Girls. \nCheck out: @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada @boutiquecannabiscanada \nBoutique Cannabis girl: @kwbabyy @mzkaylzz @allthingsamandaa @j.desireexo @apriliciouss @marishika_ \nShooter: @jasegraphics\nMUA: @swankmakeup"
 	]
 	comment = [
-				"\n•\n•\n•\n•\n•\n#guesswhatwednesday #bud #buds #giveaway #giveaways #follow #like #love #highlife #canadian #cannabis #dispensary #dabs #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #420 #710 #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life",
-				"\n•\n•\n•\n•\n•\n#cbd #shatter #cbdcrystalline #giveaway #giveaways #follow #like #love #highlife #canadian #cannabis #dispensary #dabs #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #420 #710 #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life",
 				"\n•\n•\n•\n•\n•\n#highlife #canadian #cannabis #dispensary #dabs #chronnoisseurschoice #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #bud #budtenders #420 #710 #iloveweed #weedstagram #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life #maryjane #buds #shatter #420girls",
-				# for budz for breasts
-				"\n•\n•\n•\n•\n•\n#weedconvention #fundraiser #fuckcancer #cancer #highlife #canadian #cannabis #dispensary #dabs #chronnoisseurschoice #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #bud #budtenders #420 #710 #iloveweed #weedstagram #stonernation #hightimes #ganja #bakedinbc #maryjane #buds #giveaway",
-				"\n•\n•\n•\n•\n•\n#Boutiquecannabis #boutiquecannabisgirls #weedgirls #girlswhosmokeweed #follow4follow #love #girlswithtattoos #instagood #cute #photooftheday #tbt #followme #girl #beautiful #happy #picoftheday #instadaily #fitgirls #girlswholift #amazing #Sexy #fashion #igers #fun #summer #instalike #bestoftheday #smile #like4like #instamood"
+				"\n•\n•\n•\n•\n•\n#boutiquecannabis #boutiquecannabisgirls #weedgirls #girlswhosmokeweed #follow4follow #love #girlswithtattoos #instagood #cute #photooftheday #tbt #followme #girl #beautiful #happy #picoftheday #instadaily #fitgirls #girlswholift #amazing #Sexy #fashion #igers #fun #summer #instalike #bestoftheday #smile #like4like #instamood"
 	]
+
+	guestWhatWednesdayPost = Post("Guess what wednesday")
+	guestWhatWednesdayPost.fileName = None
+	guestWhatWednesdayPost.caption = "🤔🤔🤔 GUESS the STRAIN & TAG A FRIEND to WIN $100 store credit 🎁🎁🎁 drop by every week for our weekly #guesswhatwednesdays giveaway!! Shout out to @triscuit.farms Peep us @boutiquecannabiscanada 👀"
+	guestWhatWednesdayPost.comment = "\n•\n•\n•\n•\n•\n#guesswhatwednesday #bud #buds #giveaway #giveaways #follow #like #love #highlife #canadian #cannabis #dispensary #dabs #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #420 #710 #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life"
+	guestWhatWednesdayPost.scheduledTime = None
+
+	giveawayPost = Post("28 gram giveaway")
+	giveawayPost.fileName = '28GramGiveaway.jpg'
+	giveawayPost.caption = 	"28 GRAM GIVEAWAY 🍯 💎 ❤️\nEVERY POST IS A NEW CHANCE TO WIN!\nCOMING UP ON AUGUST 28TH #28gOnThe28th\nPRIZE 👉 14g Shatter 🍯🐝 and 14g CBD Crystalline 💎💎 👇  CONTEST RULES (MUST complete all three )👇\n1️⃣. FOLLOW @boutiquecannabiscanada 👀 \n2️⃣. REPOST this picture, make sure to tag us \n3️⃣. LIKE & COMMENT below, tag friends you'd smoke with 💨\nMore friends you tag, the better your chances of winning 😀\nDM us anything to repost, we love original content 👌\nWith ❤️ from @boutiquecannabiscanada 👀"
+	giveawayPost.comment = 	"\n•\n•\n•\n•\n•\n#cbd #shatter #cbdcrystalline #giveaway #giveaways #follow #like #love #highlife #canadian #cannabis #dispensary #dabs #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #420 #710 #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life"
+	# giveawayPost.scheduledTime = '2017-08-20 10:12:50' # format: '2017-08-19 20:16:40'
+
+	budzForBreastsPost = Post("Budz for breasts")
+	budzForBreastsPost.fileName = 'BudzForBreasts.jpg'
+	budzForBreastsPost.caption = "September 2nd at @theplanetparadise is going to be an amazing evening 🎀 thank you to everyone who's contributing ❤️ @boutiquecannabiscanada 👀"
+	budzForBreastsPost.comment = "\n•\n•\n•\n•\n•\n#weedconvention #fundraiser #fuckcancer #cancer #highlife #canadian #cannabis #dispensary #dabs #chronnoisseurschoice #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #bud #budtenders #420 #710 #iloveweed #weedstagram #stonernation #hightimes #ganja #bakedinbc #maryjane #buds #giveaway"
+	# budzForBreastsPost.scheduledTime = '2017-08-20 17:20:30'
+
+	firstGirlPost = Post("First girl post")
+	firstGirlPost.fileName = 'girl1.jpg'
+	firstGirlPost.caption = "👯Two is always better than one 😉 \nCanada's most alluring women, Boutique Cannabis Girls. \nCheck out: @boutiquecannabiscanada 👀 @boutiquecannabisofficial \nBoutique Cannabis girl: @kwbabyy \nShooter: @jasegraphics\nMUA: @swankmakeup"
+	firstGirlPost.comment = "\n•\n•\n•\n•\n•\n#boutiquecannabis #boutiquecannabisgirls #girls #model #girlswhosmokeweed #weedgirls #girlswithtattoos #cute #picoftheday #beautiful #photooftheday #instagood #fun #smile #pretty #follow #hot #instagramers #potd #eyes #beauty #fit #girlswholift #girlswhosquat #fitness #instafit #canadianbabes #canadiangirls #sexy #booty"
+	# firstGirlPost.scheduledTime = '2017-08-20 13:30:30'
+
+	genericPost = Post("generic")
+	genericPost.comment = "\n•\n•\n•\n•\n•\n#highlife #canadian #cannabis #dispensary #dabs #chronnoisseurschoice #rosin #weed #weedporn #cannabiscommunity #pot #cloudsovercanada #710society #bud #budtenders #420 #710 #iloveweed #weedstagram #stonernation #hightimes #ganja #bakedinbc #terps #thc #710life #maryjane #buds #shatter #420girls"
+	genericPost.scheduledTime = '2017-08-20 23:53:00'
 
 	# drone = InstaDrone()
 	# drone.login()
-	# drone.post(fileName[0], caption[0])
-	# drone.commentPost(comment[1])
+	# drone.post(guestWhatWednesdayPost)
+	# drone.commentPost(guestWhatWednesdayPost)
 	# drone.halt()
 
-    # guess what wed
-	# sched.add_job(my_job, 'date', run_date='2017-08-16 18:30:30', kwargs={'fileName': fileName[4], 'caption': caption[4], 'comment': comment[0]})
-	# 28 gram giveaway
-	sched.add_job(my_job, 'date', run_date='2017-08-19 16:19:00', kwargs={'fileName': fileName[0], 'caption': caption[0], 'comment': comment[1]})
-	# carl sagan
-	sched.add_job(my_job, 'date', run_date='2017-08-19 19:00:00', kwargs={'fileName': fileName[9], 'caption': caption[9], 'comment': comment[2]})
-	# boutique canna girl 
-	# sched.add_job(my_job, 'date', run_date='2017-08-19 15:05:40', kwargs={'fileName': fileName[8], 'caption': caption[8], 'comment': comment[4]})
-	# edible
-	# sched.add_job(my_job, 'date', run_date='2017-08-17 17:59:00', kwargs={'fileName': fileName[6], 'caption': caption[6], 'comment': comment[2]})
-	# budz 4 breasts
-	# sched.add_job(my_job, 'date', run_date='2017-08-18 13:00:00', kwargs={'fileName': fileName[7], 'caption': caption[7], 'comment': comment[3]})
+	for post in posts:
+		if post.scheduledTime != None:
+			print('Scheduling: ' + post.title + ' for time: ' + post.scheduledTime)
+			sched.add_job(autopost, 'date', run_date=post.scheduledTime, kwargs={'post': post})
+
 	sched.start()
